5898b652018dc1e2f469430d1349595513ff13f7
Hongnee <lamhongg99@gmail.com>
2025-12-03 22:26:43 +0700

    commit

--- src/test/java/hongktl/SelectDropDownList.java
+++ src/test/java/hongktl/SelectDropDownList.java
@@ -2,6 +2,7 @@ package hongktl;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -37,20 +38,24 @@ public class SelectDropDownList {
     driver.get("https://multiple-select.wenzhixin.net.cn/templates/template.html?v=189&url=basic.html");
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.findElement(By.xpath("//label[contains(text(),' Group Select')]/parent::div[@class='mb-3 row']/preceding-sibling::div/div/div/button[@class='ms-choice']")).click();
-    driver.findElement(By.xpath("//*[@id=\"example\"]/div/div[2]/div/div/button")).click();
     WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Feb')]")));
     driver.findElement(By.xpath("//span[contains(text(),'Feb')]")).click();
+    driver.findElement(By.xpath("//p[contains(text(),'Multiple select without any options')]")).click();
 
-//    WebElement dropDown = driver.findElement(By.xpath("//label[contains(text(),' Group Select')]/parent::div[@class='mb-3 row']/preceding-sibling::div/div/div/button[@class='ms-choice']"));
-//    dropDown.click();
-//    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-//    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Feb')]")));
-//    WebElement Feb = driver.findElement(By.xpath("//span[contains(text(),'Feb')]"));
-//    Feb.click();
-//    WebElement June = driver.findElement(By.xpath("//span[contains(text(),'June')]"));
-//    June.click();
+
   }
+  @Test
+  public void dropListScroll() {
+    driver.get("https://multiple-select.wenzhixin.net.cn/templates/template.html?v=189&url=basic.html");
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.findElement(By.xpath("//label[contains(text(),'Group Select')]/parent::div/div/select/optgroup[@label='Group 1']")).click();
+
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//label[contains(text(),'Group Select')]/parent::div/div/select/optgroup[@label='Group 1']/following-sibling::optgroup[@label='Group 3']/option[text()='Option 7']")));
+    driver.findElement(By.xpath("//label[contains(text(),'Group Select')]/parent::div/div/select/optgroup[@label='Group 1']/following-sibling::optgroup[@label='Group 3']/option[text()='Option 7']")).click();
+  }
+
   @AfterClass
  public void tertDown()
  {
