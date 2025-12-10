56b73537ede105e7b06e00ff193ecee70d5bf92a
Hongnee <lamhongg99@gmail.com>
2025-12-10 22:22:24 +0700

    commit

--- src/test/java/hongktl/GetElementAttribute.java
+++ src/test/java/hongktl/GetElementAttribute.java
@@ -15,13 +15,14 @@ import java.time.Duration;
 
 public class GetElementAttribute {
     WebDriver driver;
-    private  By email = By.xpath("//label[text()='E-mail']/following-sibling::input");
-    private  By pwd = By.xpath("//label[text()='Mật khẩu']/following-sibling::input");
-    private  By pwdBtn = By.xpath("//span[contains(text(),'Đăng nhập')]");
+    private By email = By.xpath("//label[text()='E-mail']/following-sibling::input");
+    private By pwd = By.xpath("//label[text()='Mật khẩu']/following-sibling::input");
+    private By pwdBtn = By.xpath("//span[contains(text(),'Đăng nhập')]");
     String emailInValid = "emailInvalid";
     String emailValid = "tamdd1998@gmail.com";
     String pwdValid = "123456aA@";
     String pwdInvalid = "pwdInvalid";
+
     @BeforeClass
     public void setUP() throws InterruptedException {
         driver = new ChromeDriver();
@@ -29,14 +30,16 @@ public class GetElementAttribute {
         driver.get("http://103.121.90.177:8888/");
         Thread.sleep(3000);
     }
+
     @Test
-    public void verifyUI(){
-        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='Đăng nhập']")).isDisplayed(),"Label khong hien thi");
-        Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Vui lòng')]")).isDisplayed(),"Text please khong hien thi");
-        Assert.assertTrue(driver.findElement(email).isDisplayed(),"Ô Email không hiển thị");
-        Assert.assertTrue(driver.findElement(pwd).isDisplayed(),"Ô Mật khẩu  không hiển thị");
-        Assert.assertTrue(driver.findElement(pwdBtn).isDisplayed(),"Button Đăng nhập không hiển thị");
+    public void verifyUI() {
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='Đăng nhập']")).isDisplayed(), "Label khong hien thi");
+        Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Vui lòng')]")).isDisplayed(), "Text please khong hien thi");
+        Assert.assertTrue(driver.findElement(email).isDisplayed(), "Ô Email không hiển thị");
+        Assert.assertTrue(driver.findElement(pwd).isDisplayed(), "Ô Mật khẩu  không hiển thị");
+        Assert.assertTrue(driver.findElement(pwdBtn).isDisplayed(), "Button Đăng nhập không hiển thị");
     }
+
     @Test
     public void loginInValid_InputBlank() {
         driver.findElement(pwdBtn).click();
@@ -44,29 +47,33 @@ public class GetElementAttribute {
         Assert.assertEquals(msgEmail1, "Email là bắt buộc");
         Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(),'Mật khẩu là bắt buộc')]")).isDisplayed());
     }
+
     @Test
-            public void loginInavld(){
+    public void loginInvalid() {
         System.out.println("TC2: Nhập sai email ");
         driver.findElement(email).sendKeys(emailInValid);
         driver.findElement(pwd).sendKeys(pwdValid);
         driver.findElement(pwdBtn).click();
         String msgEmail2 = driver.findElement(By.xpath("//label[text()='E-mail']/parent::div/parent::div/following-sibling::div/div/div/div")).getAttribute("textContent").trim();
-        Assert.assertEquals(msgEmail2,"Chưa đúng định dạng email");
-}
+        Assert.assertEquals(msgEmail2, "Chưa đúng định dạng email");
+    }
+
     @Test
-    public void loginValid(){
+    public void loginValid() {
         //Điền thông tin valid
         System.out.println("TC1: Nhập đúng thông tin");
         driver.findElement(email).sendKeys(emailValid);
         driver.findElement(pwd).sendKeys(pwdValid);
-        Assert.assertEquals(driver.findElement(pwd).getAttribute("type"),"password");
+        Assert.assertEquals(driver.findElement(pwd).getAttribute("type"), "password");
         driver.findElement(By.xpath("//button[@aria-label='Mật khẩu appended action']")).click();
-        Assert.assertEquals(driver.findElement(pwd).getAttribute("type"),"text");
+        Assert.assertEquals(driver.findElement(pwd).getAttribute("type"), "text");
         driver.findElement(pwdBtn).click();
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement page =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Dữ liệu cơ sở']")));
+        WebElement page = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Dữ liệu cơ sở']")));
         Assert.assertTrue(page.isDisplayed());
     }
+
     @AfterClass
     public void tearDown() {
         if (driver != null) {
